d493038dae4475b00de3923d1b85d83689f1f6d6
rohansinghdeo123-sys <rohan.singhdeo123@gmail.com>
2026-03-01 17:18:28 +0530

    Configured Firebase env variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,17 +1,16 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZPsZcG32XZxAeCmg8A5V7P6vsou8U7k0",
-  authDomain: "ai-study-dashboard-56c9e.firebaseapp.com",
-  projectId: "ai-study-dashboard-56c9e",
-  storageBucket: "ai-study-dashboard-56c9e.firebasestorage.app",
-  messagingSenderId: "15263368937",
-  appId: "1:15263368937:web:38a6207c32ac56f59b9278",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-const app = !getApps().length
-  ? initializeApp(firebaseConfig)
-  : getApp();
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
